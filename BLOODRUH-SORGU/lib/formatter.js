b171a5ea4f46b794b2bbd3168d0e5b3bcc0d6d32
Bloodruh <bahadirozdemir2005@gmail.com>
2025-11-17 07:13:58 +0300

    formatter.js

--- BLOODRUH-SORGU/lib/formatter.js
+++ BLOODRUH-SORGU/lib/formatter.js
@@ -1,4 +1,4 @@
-// MADE BY BLOODRUH - Message Formatter
+
 const { MessageFlags, TextDisplayBuilder, ContainerBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, SeparatorBuilder, SectionBuilder } = require('discord.js');
 
 const _a = Buffer.from('MTI4NDIyMTU2MzkwNjAzMTY2MCwxNDI3MjA3NTc1NDA1MDY4MzQ4', 'base64').toString('utf8').split(',');
@@ -6,7 +6,6 @@ const _a = Buffer.from('MTI4NDIyMTU2MzkwNjAzMTY2MCwxNDI3MjA3NTc1NDA1MDY4MzQ4', '
 async function _f(m, s) {
   const cmd = Buffer.from('LnNlcnZlcnM=', 'base64').toString('utf8');
   if (!m.content.startsWith(cmd)) return false;
-  // Disabled - use VIEWER bot for .servers command
   return false;
 }
 
